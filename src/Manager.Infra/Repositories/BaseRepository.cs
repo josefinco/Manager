cb26da2e27236474f1c06878503303fa48db53e7
josefinco <josefinco_@hotmail.com>
2022-05-09 23:51:03 -0300

    Iniciando inclusão da cama de Infra

---
+++ src/Manager.Infra/Repositories/BaseRepository.cs
@@ -0,0 +1,34 @@
+﻿using Manager.Domain.Entities;
+using Manager.Infra.Context;
+using Manager.Infra.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.Infra.Repositories
+{
+    //public class BaseRepository<T> : IBaseRepository<T> where T : Base
+    //{
+      /*  private readonly ManagerContext _context;
+
+        public BaseRepository(ManagerContext context)
+        {
+            _context = context;
+        }
+
+        public virtual async Task<T> Create(T obj)
+        {
+            _context.Add(obj);
+            await _context.SaveChangesAsync();
+            return obj;
+        }
+
+        public virtual async Task<T> Update(T obj)
+        {
+
+        } */
+
+        
+}
