c8eb6efdc8e84ce3d43e149d98ae1e01cfe36807
josefinco <josefinco_@hotmail.com>
2022-05-11 22:40:27 -0300

    Adicionando UserMapping

---
+++ src/Manager.Infra/Mappings/UserMap.cs
@@ -0,0 +1,25 @@
+﻿using Manager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace Manager.Infra.Mappings
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+
+            //TODO: Adicionar mapeamento
+            /*builder.ToTable("User");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .UseIdentityColumn()
+                .HasColumnName("BIGINT");
+            */
+            
+
+        }
+    }
+}
