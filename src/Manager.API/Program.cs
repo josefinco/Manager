9b9eae81178f96d9c6e34984be319aa8a0a0a150
josefinco <josefinco_@hotmail.com>
2022-05-26 20:24:31 -0300

    Adicionar informações de contato no Swagger

--- src/Manager.API/Program.cs
+++ src/Manager.API/Program.cs
@@ -1,9 +1,29 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSwaggerGen(options =>
+{
+    //options.DescribeAllEnumsAsStrings();
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Manager User API´",
+        Version = "v1",
+        Description = "Api para gerência de usuários",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "José Finco",
+            Email = "josefinco_@hotmail.com",
+            Url = new Uri("https://josefinco.github.io/index.html"),
+        }
+    });
+
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
