5f6ccf1d82ce33210475699bfd2fa912b3b320a8
josefinco <josefinco_@hotmail.com>
2022-05-26 20:12:43 -0300

    Adicionar primeiro EndPoint e Classe de ViewModel para validação de objeto em camada API

---
+++ src/Manager.API/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Manager.Core.Exceptions;
+using Manager.API.ViewModels;
+
+namespace Manager.API.Controllers
+{
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [Route("api/v1/create")]
+        public async Task<IActionResult> Create([FromBody] CreateUserViewModel userViewModel)
+        {
+            try
+            {
+                return Ok();
+
+            }
+            catch (DomainException ex)
+            {
+                return BadRequest();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Erro");
+            }
+        }
+    }
+}
---
+++ src/Manager.API/ViewModels/CreateUserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Manager.API.ViewModels
+{
+    public class CreateUserViewModel
+    {
+        [Required(ErrorMessage = "O nome não pode ser nulo.")]
+        [MinLength(3, ErrorMessage = "O nome deve ter no mínimo 3 caracteres.")]
+        [MaxLength(80, ErrorMessage = "O nome deve ter no máximo 80 caracteres.")]
+        public string? Name { get; private set; }
+
+        [Required(ErrorMessage = "O email não pode ser nulo.")]
+        [MinLength(10, ErrorMessage = "O email deve ter no mínimo 10 caracteres.")]
+        [MaxLength(280, ErrorMessage = "O email deve ter no máximo 280 caracteres.")]
+        [RegularExpression(@"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+                            , ErrorMessage = "O email deve ser válido.")]
+        public string? Email { get; private set; }
+
+        [Required(ErrorMessage = "A senha não pode ser nula.")]
+        [MinLength(6, ErrorMessage = "A senha deve ter no mínimo 6 caracteres.")]
+        [MaxLength(30, ErrorMessage = "A senha deve ter no máximo 30 caracteres.")]
+        public string? Password { get; private set; }
+
+
+    }
+}
