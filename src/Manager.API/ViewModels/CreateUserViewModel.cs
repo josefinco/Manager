c690aa260e9eb0b5493c9a678ebdd7c9a857b17f
josefinco <josefinco_@hotmail.com>
2022-05-26 21:11:39 -0300

    Corrigir atributos para public

--- src/Manager.API/ViewModels/CreateUserViewModel.cs
+++ src/Manager.API/ViewModels/CreateUserViewModel.cs
@@ -7,19 +7,19 @@ namespace Manager.API.ViewModels
         [Required(ErrorMessage = "O nome não pode ser nulo.")]
         [MinLength(3, ErrorMessage = "O nome deve ter no mínimo 3 caracteres.")]
         [MaxLength(80, ErrorMessage = "O nome deve ter no máximo 80 caracteres.")]
-        public string? Name { get; private set; }
+        public string Name { get; set; }
 
         [Required(ErrorMessage = "O email não pode ser nulo.")]
         [MinLength(10, ErrorMessage = "O email deve ter no mínimo 10 caracteres.")]
         [MaxLength(280, ErrorMessage = "O email deve ter no máximo 280 caracteres.")]
         [RegularExpression(@"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
                             , ErrorMessage = "O email deve ser válido.")]
-        public string? Email { get; private set; }
+        public string? Email { get; set; }
 
         [Required(ErrorMessage = "A senha não pode ser nula.")]
         [MinLength(6, ErrorMessage = "A senha deve ter no mínimo 6 caracteres.")]
         [MaxLength(30, ErrorMessage = "A senha deve ter no máximo 30 caracteres.")]
-        public string? Password { get; private set; }
+        public string? Password { get; set; }
 
 
     }
