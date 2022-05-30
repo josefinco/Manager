7c9364e4ae09526a5142ccc6ff2063ccef0c709f
José Finco <josefinco_@hotmail.com>
2022-05-30 14:34:06 -0300

    Adicionar função OnConfigure

--- src/Manager.Infra/Context/ManagerContext.cs
+++ src/Manager.Infra/Context/ManagerContext.cs
@@ -9,18 +9,24 @@ using System.Threading.Tasks;
 
 namespace Manager.Infra.Context
 {
-    public class ManagerContext : DbContext
+  public class ManagerContext : DbContext
+  {
+    public ManagerContext() { }
+
+    public ManagerContext(DbContextOptions<ManagerContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+
+    // Configuração para execução Inicial do Banco de Dados
+    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    // {
+    //   optionsBuilder.UseSqlServer(@"Server = 127.0.0.1; Database = USERMANAGERAPI; User Id = admin; Password = Admin@123456");
+    // }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        public ManagerContext() { }
-
-        public ManagerContext(DbContextOptions<ManagerContext> options) : base(options) { }
-
-        public DbSet<User> Users { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.ApplyConfiguration(new UserMap());
-        }
-
+      modelBuilder.ApplyConfiguration(new UserMap());
     }
+
+  }
 }
