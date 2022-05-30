8de3eb048fe3d6025211b3321fcaa5c552882ebb
José Finco <josefinco_@hotmail.com>
2022-05-30 16:04:38 -0300

    Adicionar JsonIgnore no parâmetro password

--- src/Manager.Services/DTO/UserDTO.cs
+++ src/Manager.Services/DTO/UserDTO.cs
@@ -2,26 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Manager.Services.DTO
 {
-    public class UserDTO
+  public class UserDTO
+  {
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    [JsonIgnore]
+    public string? Password { get; set; }
+
+    public UserDTO()
+    { }
+
+    public UserDTO(long id, string? name, string? email, string? password)
     {
-        public long Id { get; set; }
-        public string? Name { get; set; }
-        public string? Email { get; set; }
-        public string? Password { get; set; }
-
-        public UserDTO()
-        {}
-
-        public UserDTO(long id, string? name, string? email, string? password)
-        {
-            Id = id;
-            Name = name;
-            Email = email;
-            Password = password;
-        }
+      Id = id;
+      Name = name;
+      Email = email;
+      Password = password;
     }
+  }
 }
