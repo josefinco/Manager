2fecb0ca491ce62c3472025f54f770d4b33dceac
josefinco <josefinco_@hotmail.com>
2022-06-01 15:11:42 -0300

    Adicionar testes

---
+++ src/Manager.Tests/Configuration/AutoMapperConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Manager.Domain.Entities;
+using Manager.Services.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.Tests.Configuration
+{
+    public static class AutoMapperConfiguration
+    {
+
+        public static IMapper Configure()
+        {
+            var autoMapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<User, UserDTO>()
+                .ReverseMap();
+            });
+
+            return autoMapperConfiguration.CreateMapper();
+        }
+
+
+    }
+}
